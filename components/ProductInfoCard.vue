96df2da89ba3a5ee50362d6f298ff79529902dc7
moose <moosecodes@gmail.com>
2023-07-05 10:50:20 -0700

    Add ProductInfoCard component

---
+++ components/ProductInfoCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <p><b>{{ product.title }}</b></p>
+      <p><b>${{product.price}}</b></p>
+      <p><img :src="product.thumbnail" /></p>
+      <p>{{product.description}}</p>
+      <p>{{product.stock > 0 ? '' : 'Out of Stock' }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+    const { product } = defineProps(['product'])
+  </script>
+  
+  <style scoped>
+
+  </style>
