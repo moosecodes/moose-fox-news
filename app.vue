19427d6fe8a2a7fa3e28c4ddcdfc11a63897203e
moose <moosecodes@gmail.com>
2023-07-05 10:50:45 -0700

    Fetch data from dummyjson.com

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div v-if="data.products">
+      <div v-for="p in data.products" :key="p.id">
+        <ProductInfoCard :product="p" />
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+  const { data } = await useFetch('https://dummyjson.com/products')
+  if(!data.value.products.length) {
+    // handle error
+  }
+</script>
